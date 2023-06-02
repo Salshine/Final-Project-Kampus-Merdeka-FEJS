832c1c3ecc49c49ef8b1a8ebd66c48928c7f2b47
Shintya Dian <shintyadianp@gmail.com>
2023-06-02 21:48:38 +0700

    add dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
   );
